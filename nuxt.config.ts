ed1fd51d4fdf076fdc2fbff7db41274380522a1f
Ricardo Bernardes <ricardo.bernardes@webtales.fr>
2023-03-01 01:21:37 +0000

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        baseURL: '/nuxt-tanto-na-lingua/' // baseURL: '/<repository>/'
+    },
+
     modules: ['@nuxtjs/tailwindcss'],
 
     css: ["~/assets/css/tailwind.css"],
