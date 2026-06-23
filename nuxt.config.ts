8b574facd1a8a134aeceefc788d36b206189fcb3
Ricardo Bernardes <ricardo.bernardes@webtales.fr>
2026-06-23 12:17:57 +0100

    feat: change GA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,8 @@ export default defineNuxtConfig({
             charset: 'utf-8',
             viewport: 'width=device-width, initial-scale=1',
             script: [
-                {   async: true, src: "https://www.googletagmanager.com/gtag/js?id=G-1K35XKEL7N", defer: true }
+                { async: true, src: "https://www.googletagmanager.com/gtag/js?id=G-03BH181FTM" },
+                { innerHTML: "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-03BH181FTM');" }
             ]
         },
         baseURL: '/' // baseURL: '/<repository>/'
