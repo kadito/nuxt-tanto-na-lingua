439276d000ff51ba33e612f5191e6bebc2d445d6
Ricardo Bernardes <ricardo.bernardes@webtales.fr>
2026-06-23 22:51:23 +0100

    feat: consent mode update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,7 +48,9 @@ _iub.csConfiguration = {
                 { async: true, src: "//cdn.iubenda.com/cs/iubenda_cs.js" },
                 // 3. Iubenda embed script (for policy links)
                 { innerHTML: "(function (w,d) {var loader = function () {var s = d.createElement('script'), tag = d.getElementsByTagName('script')[0]; s.src='https://cdn.iubenda.com/iubenda.js'; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener('load', loader, false);}else if(w.attachEvent){w.attachEvent('onload', loader);}else{w.onload = loader;}})(window, document);" },
-                // 4. Google Analytics
+                // 4. Iubenda consent widget (floating preferences button)
+                { src: "https://embeds.iubenda.com/widgets/1d838166-f912-43d1-ac64-b536b339dbb6.js" },
+                // 5. Google Analytics
                 { async: true, src: "https://www.googletagmanager.com/gtag/js?id=G-03BH181FTM" },
                 { innerHTML: "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-03BH181FTM');" }
             ]
